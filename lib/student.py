03c09e4dd6e04ece161d537ecd5d778878afad31
KIPROTYCH <kiprotych@gmail.com>
2023-08-23 12:18:16 +0300

    Completed Assignment

--- lib/student.py
+++ lib/student.py
@@ -7,9 +7,10 @@ class Student:
 
 class ChattyStudent(Student):
     def hello(self):
-        super().hello()  # Inherit hello() method from the parent class
+        super().hello() 
         print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
 
     def raise_hand(self):
         for _ in range(10):
-            super().raise_hand() # Call super() to inherit and print "Pick me!" ten times
+            
+            super().raise_hand()
